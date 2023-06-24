9b6aec28c2bdf51f2f41606888e2798658426454
Samya Bikash Santra <samya.santra@gmail.com>
2023-06-24 07:49:56 +0530

    first commit

---
+++ src/main/java/com/samya/aws/cicd/example/Order.java
@@ -0,0 +1,17 @@
+package com.samya.aws.cicd.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Order {
+    private int id;
+    private String name;
+    private double price;
+    private int quantity;
+}
---
+++ src/main/java/com/samya/aws/cicd/example/OrderServiceApplication.java
@@ -0,0 +1,27 @@
+package com.samya.aws.cicd.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/orders")
+public class OrderServiceApplication {
+
+    @Autowired
+    private OrderDao orderDao;
+
+    public List<Order> fetchOrders(){
+        return orderDao.getOrders();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+}
