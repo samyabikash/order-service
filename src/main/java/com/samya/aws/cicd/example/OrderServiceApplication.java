bbb16cd5d3509362ad8b02d44e63071214da1c46
Samya Bikash Santra <samya.santra@gmail.com>
2023-06-24 22:09:15 +0530

    Adding @GetMapping

--- src/main/java/com/samya/aws/cicd/example/OrderServiceApplication.java
+++ src/main/java/com/samya/aws/cicd/example/OrderServiceApplication.java
@@ -3,6 +3,7 @@ package com.samya.aws.cicd.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class OrderServiceApplication {
     @Autowired
     private OrderDao orderDao;
 
+    @GetMapping
     public List<Order> fetchOrders(){
         return orderDao.getOrders();
     }
